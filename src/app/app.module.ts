b295b534d357c2b4ee253fb79aaa7646e68810f7
Ashok <ashokpatidar80@gmail.com>
2019-11-26 21:49:36 +0530

    ⚽ configure mongoose ⚽

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const mongoUrl: string =
+  process.env.MONGO_DB || 'mongodb://localhost:27017/nest-project';
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(mongoUrl)],
   controllers: [AppController],
   providers: [AppService],
 })
