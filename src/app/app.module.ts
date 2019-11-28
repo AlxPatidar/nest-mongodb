5470382775b9c4202a6068523da7463e850827c0
Ashok <ashokpatidar80@gmail.com>
2019-11-28 12:49:40 +0530

    🗁 change dirctory structure 🗀

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiModule } from '../api/api.module';
-import { UsersModule } from '../users/users.module';
-import { AuthModule } from '../auth/auth.module';
 
 const mongoUrl: string =
   process.env.MONGO_DB || 'mongodb://localhost:27017/nest-project';
@@ -12,8 +10,6 @@ const mongoUrl: string =
 @Module({
   imports: [
     ApiModule,
-    UsersModule,
-    AuthModule,
     MongooseModule.forRoot(mongoUrl, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
