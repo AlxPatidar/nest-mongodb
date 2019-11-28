91129e1423e36b8e904d2d4e8a1110fe9671a331
Ashok <ashokpatidar80@gmail.com>
2019-11-28 08:58:45 +0530

    📂 change directory structure 📁

--- src/api/task/task.module.ts
+++ src/api/task/task.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
-import { TaskSchema } from './task.schema';
+import { TaskSchema } from './schema/task.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'Task', schema: TaskSchema }])],
