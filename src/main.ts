3a63c291a35deb181f9c3d8ccf54d7886d877d89
Ashok <ashokpatidar80@gmail.com>
2019-11-29 22:23:47 +0530

    🎫 custom logger 🎫

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import * as dotEnv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { Logger } from '@nestjs/common';
+import { CustomeLogger } from './shared/Logger/log.logger';
 
 // Configure environment file with project
 dotEnv.config();
@@ -11,8 +12,11 @@ const port = process.env.PORT || 4001;
 
 async function bootstrap() {
   // Create nestFactory instance for make server instance
-  const app = await NestFactory.create(AppModule);
-  Logger.log(`🚀  Server ready at http://localhost:${port} `);
+  const app = await NestFactory.create(AppModule, {
+    logger: new CustomeLogger(),
+  });
+
+  Logger.log(`🚀  Server ready at http://localhost:${port} `, 'ServerStarted');
   // Run server on port
   await app.listen(port);
 }
